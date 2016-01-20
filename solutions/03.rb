023f30697a337e13ac3636979e360b20c7610a2f
Georgi Stefanov <georgisvetlozarovstefanov@abv.bg>
2016-01-20 13:08:47 +0200

    Made wholesome changes to code
    - Removed repetitive code checking if a number is prime.
    - Added new implementation for generating next number in a sequence.

--- solutions/03.rb
+++ solutions/03.rb
@@ -1,106 +1,99 @@
-def prime?(number)
-  divisors = 0
+class Integer
+  def prime?
+    return false if self <= 1
+    divisors = 0
 
-  2.upto(number - 1).each {|x| divisors += 1 if number % x == 0  }
-
-  case number
-    when 0 then false
-    when 1 then false
-    else number == 2 or divisors == 0
+    2.upto(Math.sqrt(self)).each do |divisor|
+      return false if self % divisor == 0
+    end
+    true
   end
 end
-def prime?(number)
-  divisors = 0
-
-  2.upto(number - 1).each {|x| divisors += 1 if number % x == 0  }
-
-  case number
-    when 0 then false
-    when 1 then false
-    else number == 2 or divisors == 0
-  end
-end
-
-def increasing_numerator(numerator, denominator)
-    numerator += 1
-    denominator -= 1 unless denominator == 1
-
-    return numerator, denominator if numerator.gcd(denominator) == 1
-    return increasing_numerator(numerator, denominator) if numerator.gcd(denominator) > 1
- end
-
-def decreasing_numerator(numerator, denominator)
-  numerator -= 1 unless numerator == 1
-  denominator += 1
-
-  return numerator, denominator if numerator.gcd(denominator) == 1
-  return decreasing_numerator(numerator, denominator) if numerator.gcd(denominator) > 1
-end
-
-def rational_generator(n)
-  return  Rational(1,1) if n ==  1
-
-  denominator = rational_generator(n - 1).denominator
-  numerator = rational_generator(n - 1).numerator
-
-  if (numerator + denominator) % 2 == 0
-    numerator_incremented = increasing_numerator(numerator, denominator)[0]
-    denominator_incremented = increasing_numerator(numerator, denominator)[1]
-  else
-    numerator_incremented = decreasing_numerator(numerator, denominator)[0]
-    denominator_incremented = decreasing_numerator(numerator, denominator)[1]
-  end
-
-  Rational(numerator_incremented, denominator_incremented)
-end
-
-def nth_fibonacci(place, first: 1, second: 1)
-  case place
-    when 0 then 0
-    when 1 then first
-    when 2 then second
-
-    else
-  fibonacci_first_predecessor = nth_fibonacci(place - 1, first: first, second: second)
-  fibonacci_second_predecessor = nth_fibonacci(place - 2, first: first, second: second)
-  fibonacci_first_predecessor + fibonacci_second_predecessor
-  end
-end
-
 
 class PrimeSequence
   include Enumerable
 
-  def initialize(number)
+  def initialize(size)
     infinity = 1.0 / 0
-    @sequence = (1..infinity).lazy.select{|n| prime?(n)}.take(number)
+    @sequence = (1..infinity).lazy.select{ |number| number.prime? }.take(size)
   end
 
   def each(&block)
     @sequence.each(&block)
-   end
+  end
 end
 
 
 class FibonacciSequence
   include Enumerable
 
-  def initialize(number, first: 1, second: 1)
-    @sequence = 1.upto(number).map {|n| nth_fibonacci(n, first: first, second: second )}
+  def self.nth_fibonacci(place, first: 1, second: 1)
+    case place
+      when 0 then 0
+      when 1 then first
+      when 2 then second
+
+    else
+      fibonacci_first_predecessor = nth_fibonacci(place - 1, first: first, second: second)
+      fibonacci_second_predecessor = nth_fibonacci(place - 2, first: first, second: second)
+      fibonacci_first_predecessor + fibonacci_second_predecessor
+    end
+  end
+
+  def initialize(size, first: 1, second: 1)
+    @sequence = []
+    return @sequence if size == 0
+
+    while @sequence.size < size
+      @sequence.push(first)
+  
+      new_number = generate_next(first, second)
+      first = second
+      second = new_number
+    end
+    @sequence
   end
 
   def each(&block)
     @sequence.each(&block)
   end
 
+  def generate_next(first, second)
+    first + second
+  end
 end
 
 
 class RationalSequence
   include Enumerable
 
-  def initialize(number)
-    @sequence = 1.upto(number).map {|n| rational_generator(n)}
+  def generate_next(num, denom)
+    return Rational(num, denom + 1) if num == 1 and denom % 2 == 0
+    return Rational(num + 1, denom) if num % 2 == 1 and denom == 1
+    if num % 2 == denom % 2
+      num += 1
+      denom -= 1
+    else
+      num -= 1
+      denom += 1
+    end
+    rat = Rational(num, denom)
+    return rat if rat.numerator == num and rat.denominator == denom
+    generate_next(num,denom)
+  end
+
+  def initialize(size)
+    @sequence = []
+    return @sequence if size == 0
+    num, denom = 1, 1
+    @sequence.push(Rational(num, denom))
+    while @sequence.size < size
+      number = generate_next(num, denom)
+      @sequence.push(number)
+      num = number.numerator
+      denom = number.denominator
+    end
+    @sequence
   end
 
   def each(&block)
@@ -110,8 +103,7 @@ class RationalSequence
   def sum
     @sequence.to_a.reduce(0, :+)
   end
-
-  end
+end
 
 
 module DrunkenMathematician
@@ -119,8 +111,8 @@ module DrunkenMathematician
 
   def meaningless(n)
     n_rationals = RationalSequence.new(n)
-    group_1 = n_rationals.select {|x| prime?(x.numerator) or prime?(x.denominator)}
-    group_2 = n_rationals.select {|x| not (prime?(x.numerator) or prime?(x.denominator))}
+    group_1 = n_rationals.select {|x| x.numerator.prime? or x.denominator.prime? }
+    group_2 = n_rationals.select {|x| not (x.numerator.prime? or x.denominator.prime?) }
 
     group_1.reduce(1, :*) / group_2.reduce(1, :*)
   end
@@ -133,8 +125,9 @@ module DrunkenMathematician
   end
 
   def worthless(n)
+    target = FibonacciSequence.nth_fibonacci(n)
     number = 0
-    number += 1 until RationalSequence.new(number).sum > nth_fibonacci(n)
+    number += 1 until RationalSequence.new(number).sum > target
 
     RationalSequence.new(number - 1).to_a
     end
